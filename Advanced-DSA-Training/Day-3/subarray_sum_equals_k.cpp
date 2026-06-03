326225ed7a89793739958077dd8c43ea7c3a9ef2
Sanjay Baskar <sanjaybaskar.in@gmail.com>
2026-06-03 19:43:13 +0530

    updated june 3

---
+++ Advanced-DSA-Training/Day-3/subarray_sum_equals_k.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main(){
+    // input declaration
+    vector<int> arr;
+    int k;
+
+    int n = arr.size();
+    int count = 0;
+    vector<int> prefixsum(n, 0);
+
+    prefixsum[0] = arr[0];
+    for(int i = 0; i < n; i++){
+        prefixsum[i] = prefixsum[i-1] + arr[i];
+    }
+
+    unordered_map<int, int> m;
+
+    for(int j = 0; j < n; j++){
+        if(prefixsum[j] == k) count++;
+
+        int val = prefixsum[j] - k;
+        if(m.find(val) != m.end()){
+            count += m[val];
+        }
+
+        if(m.find(prefixsum[j]) == m.end()){
+            m[prefixsum[j]] = 0;
+        }
+        m[prefixsum[j]]++;
+    }
+    cout << count;
+    return 0;
+}
